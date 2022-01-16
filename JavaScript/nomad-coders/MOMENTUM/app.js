cd6c071850298f212d27f8a307cff0ba6c625468
kssgit <76714467+kssgit@users.noreply.github.com>
2022-01-16 20:10:31 +0900

    Update app.js

--- JavaScript/nomad-coders/MOMENTUM/app.js
+++ JavaScript/nomad-coders/MOMENTUM/app.js
@@ -1,4 +1,4 @@
-// 기본적인 변수 선언 
+// 기본적인 변수 선언
 //const : 상수 선언
 //let : 변수 선언(값 변경 가능)
 const a = 10;
@@ -8,10 +8,10 @@ let myName = "성수";
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
-console.log("hello"+myName);
+console.log("hello" + myName);
 
-myName ="김성수";
-console.log("your new name is"+myName);
+myName = "김성수";
+console.log("your new name is" + myName);
 
 //boolean
 
@@ -26,33 +26,44 @@ const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 //Get Item from Array
 console.log(daysOfWeek[4]);
 
-
 // Add one more dat to the array
 daysOfWeek.push("sun");
 
-console.log(daysOfWeek)
+console.log(daysOfWeek);
 
-//Object 
+//Object
 const playerName = "nico";
 const playerPoints = 12123213;
 const playerHandsome = false;
 
-const player ={
-    name : "nico",
-    points : 10,
-    fat : true,
-}
+const player = {
+  name: "nico",
+  points: 10,
+  fat: true,
+};
 
 console.log(player);
 console.log(player.name);
 console.log(player["points"]);
-player.points = 11; //const로 선언된 Object의 변수는 변경 가능 
-console.log(player["points"]); // 하지만 player의 값을 바꾸는 건 불가능 player = 10 - > X 
+player.points = 11; //const로 선언된 Object의 변수는 변경 가능
+console.log(player["points"]); // 하지만 player의 값을 바꾸는 건 불가능 player = 10 - > X
 player.lastName = "potato";
 console.log(player); //변수 추가 가능
 
 //function
 
-function sayHello(name){
-    console.log("Hello my name is "+name);
+function sayHello(name, age) {
+  console.log("Hello my name is " + name + " and I'm " + age);
 }
+
+sayHello("nico", 10);
+
+//objec 안에 메서드
+const playe = {
+  name: "nico",
+  sayHello: function (otherPersonName) {
+    console.log("hello" + otherPersonName);
+  },
+};
+
+player.sayHello("lym");
